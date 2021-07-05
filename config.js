5b9427edae9e0962e30c08fbe72c68d9cbf9e4cf
rishabhkapar <rishabhkapar.unofficial@gmail.com>
2021-07-05 20:58:46 +0530

    MongoDB Atlas set up

---
+++ config.js
@@ -0,0 +1,6 @@
+const PORT = 7777;
+const MONGO_URL = "mongodb://localhost:27017/";
+const DB_NAME = "onlinejudge";
+const DB_URL = "mongodb+srv://OnlineJudge:OnlineJudge@onlinejudge.iltfg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+module.exports = { PORT, DB_URL };
