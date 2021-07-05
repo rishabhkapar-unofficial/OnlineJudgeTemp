b05175ce465c1732eb9c39fcf2feb62b91a3fe74
rishabhkapar <rishabhkapar.unofficial@gmail.com>
2021-07-05 21:03:36 +0530

    Fixed port issue

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const PORT = 7777;
+const PORT = process.env.PORT || 7777;
 const MONGO_URL = "mongodb://localhost:27017/";
 const DB_NAME = "onlinejudge";
 const DB_URL = "mongodb+srv://OnlineJudge:OnlineJudge@onlinejudge.iltfg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
